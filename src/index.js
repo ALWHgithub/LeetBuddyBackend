927b9482bb8b3fd00873afd520007bcb8c7317a0
AaronLi <alwhgithub@gmail.com>
2023-07-01 19:18:41 +0800

    added 2-way data flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const axios = require('axios');
-
+const bodyParser = require('body-parser')
 const app = express();
 
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -10,7 +15,10 @@ app.use((req, res, next) => {
     next();
   });
 
-app.get('/', (req, res) => {
+app.post('/',urlencodedParser, (req, res) => {
+    const receivedData = req.body; // Extract the data from the request body
+    console.log(receivedData);
+
     axios.get('https://leetcode.com/graphql?query={%20question(titleSlug:%20%22two-sum%22)%20{%20questionId%20questionFrontendId%20title%20titleSlug%20isPaidOnly%20difficulty%20likes%20dislikes%20}%20}')
     .then(response => {
         res.send(response.data)
